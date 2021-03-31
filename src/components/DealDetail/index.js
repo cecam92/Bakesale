ed1276c6f05bd4a84ebe83d1dd0bcb99dc762a85
Cesar Martinez <cecam92@hotmail.com>
2021-03-31 19:12:20 +1100

    swipe picture deal done

--- src/components/DealDetail/index.js
+++ src/components/DealDetail/index.js
@@ -1,5 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Image} from 'react-native';
+import {
+  ScrollView,
+  View,
+  Text,
+  Image,
+  PanResponder,
+  Animated,
+  Dimensions,
+} from 'react-native';
 import {priceDisplay} from '../Utils';
 import {style} from './styles';
 
@@ -8,19 +16,74 @@ import {fetchDetail} from '../../ajax';
 const DealDetail = ({route}) => {
   const {deal} = route.params;
   const [data, setData] = useState([]);
+  const [imageIndex, setImageIndex] = useState(0);
+  const [direction, setDirection] = useState(null);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const imageXPos = new Animated.Value(0);
+  const width = Dimensions.get('window').width;
 
-  const getData = async () => {
-    const detail = await fetchDetail(deal.key);
-    setData(detail);
+  const imagePanResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (evt, gs) => {
+      imageXPos.setValue(gs.dx);
+    },
+    onPanResponderRelease: (evt, gs) => {
+      if (Math.abs(gs.dx) > width * 0.2) {
+        const side = Math.sign(gs.dx);
+        setDirection(side);
+        Animated.timing(imageXPos, {
+          toValue: side * width,
+          duration: 250,
+          useNativeDriver: false,
+        }).start(() => {
+          handleSwipe(-1 * side);
+        });
+      } else {
+        Animated.spring(imageXPos, {
+          toValue: 0,
+          useNativeDriver: false,
+        }).start();
+      }
+    },
+  });
+
+  const handleSwipe = indexDirection => {
+    const media = [...deal.media];
+    if (!media[imageIndex + indexDirection]) {
+      Animated.spring(imageXPos, {
+        toValue: 0,
+        useNativeDriver: false,
+      }).start();
+      return;
+    } else {
+      setImageIndex(imageIndex + indexDirection);
+    }
   };
+
   useEffect(() => {
+    const getData = async () => {
+      const detail = await fetchDetail(deal.key);
+      setData(detail);
+    };
     getData();
-    console.log('data', data);
-  }, [data]);
+  }, [deal.key]);
+
+  useEffect(() => {
+    //Next Image Animation
+    imageXPos.setValue(-1 * direction * width);
+    Animated.spring(imageXPos, {
+      toValue: 0,
+      useNativeDriver: false,
+    }).start();
+  }, [imageIndex, direction, imageXPos, width]);
 
   return (
-    <View View style={style.deal}>
-      <Image style={style.image} source={{uri: deal.media[0]}} />
+    <ScrollView View style={style.deal}>
+      <Animated.Image
+        {...imagePanResponder.panHandlers}
+        style={[{left: imageXPos}, style.image]}
+        source={{uri: deal.media[imageIndex]}}
+      />
       <Text style={style.title}>{deal.title}</Text>
 
       <View style={style.dealDetail}>
@@ -40,7 +103,7 @@ const DealDetail = ({route}) => {
           <Text style={style.descriptionText}>{data.description}</Text>
         </View>
       )}
-    </View>
+    </ScrollView>
   );
 };
 
